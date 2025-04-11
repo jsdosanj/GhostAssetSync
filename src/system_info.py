1443a0222ed6656048182f123496b3f47ba8d236
Jasvant Singh Dosanjh <jasvantdosanjh@outlook.com>
2025-04-11 12:26:36 -0700

    Create system_info.py
    integrating logic from marksman

---
+++ src/system_info.py
@@ -0,0 +1,30 @@
+import platform, subprocess, socket, uuid
+
+def get_serial():
+    try:
+        if platform.system() == "Windows":
+            return subprocess.check_output("wmic bios get serialnumber", shell=True).decode().split("\n")[1].strip()
+        return subprocess.check_output("system_profiler SPHardwareDataType | awk '/Serial/ {print $4}'", shell=True).decode().strip()
+    except:
+        return "UNKNOWN"
+
+def get_mac_address():
+    mac = hex(uuid.getnode()).replace('0x', '').upper()
+    return ':'.join(mac[i:i+2] for i in range(0, 12, 2))
+
+def get_logged_in_users():
+    try:
+        return subprocess.check_output("query user" if platform.system() == "Windows" else "users", shell=True).decode().strip()
+    except:
+        return "UNKNOWN"
+
+def collect_system_info():
+    return {
+        "hostname": socket.gethostname(),
+        "serial": get_serial(),
+        "mac_address": get_mac_address(),
+        "os_name": platform.system(),
+        "os_version": platform.version(),
+        "os_build": platform.release(),
+        "logged_in_users": get_logged_in_users()
+    }
